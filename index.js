e61904b5d341302afb027eb937f53396be7a76c4
MonsterMannen <viktorsvensson94@gmail.com>
2017-11-17 22:38:51 +0100

    added stuff

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const https = require("https");
 const API_KEY = require('./apikey').API_KEY;
+const rankToValue = require("./rankValues").VALUES;
 const normalBlindPickID = 430;
 const normalDraftID = 400;
 const rankedSoloID = 9999;  // TODO change this sometime later (probably never)
@@ -10,6 +11,7 @@ var server = "";
 var matchIDs = [];
 var normalGames = [];
 var teammatesAndEnemies = [];
+var ranks = [];
 
 function go(name, serv){
     summonerName = name;
@@ -46,8 +48,8 @@ function matchHistory(){
         });
         res.on("end", end => {
             body = JSON.parse(body);
-            console.log("-------------");
-            console.log(body);  // debug
+            //console.log("-------------");
+            //console.log(body);  // debug
             for(var i = 0; i < body.endIndex; i++){
                 matchIDs.push(body.matches[i].gameId);
                 // add normal blind pick and draft games
@@ -56,6 +58,7 @@ function matchHistory(){
                     normalGames.push(body.matches[i].gameId);
                 }
             }
+            console.log("normal games IDs:");
             console.log(normalGames);
             getAllUsers();
         });
@@ -82,12 +85,62 @@ function getAllUsers(){
                 // this solution is insanely bad. callbacks are hard.
                 if(x++ == normalGames.length - 1){
                     // last loop
+                    console.log("-------------------------------");
                     console.log("Found users: " + teammatesAndEnemies.length);
+                    //console.log("user summonerIDs:");
+                    //console.log(teammatesAndEnemies);
                     // -> continue to next function
+                    getRankForAllUsers();
                 }
             });
         });
     }
+}
+
+function getRankForAllUsers(){
+    var x = 0;
+    for(var i = 0; i < teammatesAndEnemies.length; i++){
+        var url = "https://euw1.api.riotgames.com/lol/league/v3/positions/by-summoner/";
+        url += teammatesAndEnemies[i] + "?api_key=" + API_KEY;
+        https.get(url, res => {
+            res.setEncoding('utf8');
+            var body = "";
+            res.on("data", data => {
+                body += data;
+            });
+            res.on("end", end => {
+                body = JSON.parse(body);
+
+                if(JSON.stringify(body) != '[]'){
+                    var l = body.length;
+                    if(l !== "undefined"){
+                        // loop through different queue types
+                        for(var j = 0; j < l; j++){
+                            if(body[j].queueType === "RANKED_SOLO_5x5"){
+                                var rank = body[j].tier + " " + body[j].rank; // ex DIAMOND III
+                                ranks.push(rank);
+                            }
+                        }
+                    }
+                }
+
+                if(x++ == teammatesAndEnemies.length - 1){
+                    // last loop ( 200iq solution. or 10iq solution :c )
+                    rankValues();
+                }
+            });
+        });
+    }
+}
+
+function rankValues(){
+    var sum = 0;
+    for(var i = 0; i < ranks.length; i++){
+        sum += rankToValue[ranks[i]];
+    }
+    console.log("ranks found: " + ranks.length);
+    console.log("sum of ranks: " + sum);
+    console.log("avg rank: " + (sum/ranks.length));   // calculate this to a rank
 }
 
 // the start of everything
---
+++ rankValues.js
@@ -0,0 +1,11 @@
+const VALUES = {
+    "BRONZE V": 1,
+    "BRONZE IV": 2,
+    "BRONZE III": 3,
+    "BRONZE II": 4,
+    "BRONZE I": 5,
+    "SILVER V": 6
+    // TODO add all zzzzz
+};
+
+exports.name = VALUES;
